fd9b1b1eab6b0646ae6df2c7917437f0251be954
Proencajoao <joaoproenca1910@gmail.com>
2022-03-25 22:44:44 -0300

    Inicio projeto Calculo de vida perdida com os cigarros

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+quantidadeCigarro = 0;
+quantidadeAnos = 0;
+totalMinutosP = 0;
+
+totalHorasP = 0;
+totalDiasP = 0;
+totalMesesP = 0;
+totalAnoP = 0;
+
+resto = 0;
+
+
+
   constructor() {}
 
+  calculo(){
+    
+      /*let anos = 4; 
+      let anosInteiro = Math.floor(anos);
+      let resto = anos - anosInteiro;
+      
+      console.log(resto);
+      */
+
+
+
+
+      this.totalMinutosP = this.quantidadeCigarro * 360 * this.quantidadeAnos * 10;
+    
+      this.totalHorasP = this.totalMinutosP / 60;
+
+      this.totalDiasP = this.totalHorasP /24;
+
+      this.totalMesesP = this.totalDiasP / 30;
+
+      this.totalAnoP = this.totalMesesP / 12;
+
+  }
 }
+  
